de39f7ae7f9ab39f58f0500d909269d533c457c1
randall.erasmus <randallerasmus1@gmail.com>
2021-09-26 23:34:58 +0200

    Getting the even numbers in the list

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class main {
     public static void main(String[] args) {
@@ -12,5 +13,14 @@ public class main {
         listTest.add(7);
         System.out.println(listTest);
 
+        // Getting the even number in the list
+        List<Integer> newList = new ArrayList<>();
+
+        for (Integer numList : listTest) {
+            if (numList % 2 == 0) {
+                newList.add(numList);
+            }
+        }
+        System.out.println(newList);
     }
 }
