2523b71f68fc03823823c3e9f78800f821467cd8
randall.erasmus <randallerasmus1@gmail.com>
2021-09-26 23:32:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+In this Tutorial I will revisit the streams api in java
+This is a very reliable api that we can use to make coding
+much easier.
+
+
---
+++ src/main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(10);
+        listTest.add(24);
+        listTest.add(8);
+        listTest.add(13);
+        listTest.add(7);
+        System.out.println(listTest);
+
+    }
+}
